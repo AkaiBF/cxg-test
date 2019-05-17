0b075c41bddab70b6cf88f0b20d7dd8a15e513b5
Ernesto Echeverría González <ernestoecheverria@itop.es>
2019-05-17 15:07:24 +0100

    Components & Navbar

---
+++ src/app/app.language.ts
@@ -0,0 +1,15 @@
+export const language = {
+	es: {
+		Noticias: "Noticias",
+		Equipo: "Equipo",
+		Tienda: "Tienda",
+		Patrocinadores: "Patrocinadores"
+	}, 
+	en: {
+		Noticias: "News",
+		Equipo: "Team",
+		Tienda: "Store",
+		Patrocinadores: "Partners"
+	}
+	
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreComponent } from './components/store/store.component';
+import { TeamComponent } from './components/team/team.component';
+import { NewsComponent } from './components/news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreComponent,
+    TeamComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule
